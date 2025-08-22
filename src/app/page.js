b905d9c667664490ef2732d6289ae04f16012476
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-08-22 10:58:11 +0600

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ProductHighlights from "./components/ProductHighlights";
@@ -10,7 +11,7 @@ export default function HomePage() {
         <Hero></Hero>
         <ProductHighlights></ProductHighlights>
       </main>
-      
+      <Footer></Footer>
     </>
   );
 }
